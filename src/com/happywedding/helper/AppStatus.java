897a02bf2865b48a230081afeaaa303bd6e01347
Lê Phước Thịnh <thinhlppc03231@fpt.edu.vn>
2022-11-09 23:24:31 +0700

    upload project

---
+++ src/com/happywedding/helper/AppStatus.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.happywedding.helper;
+
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class AppStatus {
+
+//    /**
+//     * Đối tượng này chứa thông tin người sử dụng sau khi đăng nhập
+//     */
+//    public static Employee USER = null;
+//    public static int STATUS = 0;
+//    public static EmployeeManager  employeeManager  = null;
+// 
+//    //0 is not load
+//    //1 is loading
+//    
+//    public static void loadApp(){
+//        STATUS = 1;
+//    }
+//    
+//    
+//    
+//    
+//    public static void logoff() {
+//        AppStatus.USER = null;
+//    }
+//    
+//    public static boolean isFirstStart(){
+//        return STATUS == 1;
+//    }
+//
+//    /**
+//     * Kiểm tra xem đăng nhập hay chưa
+//     *
+//     * @return đăng nhập hay chưa
+//     */
+//    public static boolean authenticated() {
+//        return AppStatus.USER != null;
+//    }
+    
+//    public static String currentTask;
+//    public static String previousTask;
+}
---
+++ src/com/happywedding/manager/QuanLyTaiKhoan.java
@@ -0,0 +1,20 @@
+package com.happywedding.manager;
+
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+public class QuanLyTaiKhoan extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public QuanLyTaiKhoan() {
+		setLayout(null);
+		System.out.print("Hello");
+		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton.setBounds(321, 102, 115, 21);
+		add(tglbtnNewToggleButton);
+
+	}
+
+}
