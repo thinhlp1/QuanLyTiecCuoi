e75c5796cce9bb1017fcc1d96efe8a093c7860ac
Mai Quoc Bao <maiquocbao666@gmail.com>
2022-12-02 17:38:46 +0700

    push de pull

--- src/com/happywedding/view/manage/QuenMatKhau.java
+++ src/com/happywedding/view/manage/QuenMatKhau.java
@@ -7,11 +7,22 @@ package com.happywedding.view.manage;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author ACER
  */
-public class QuenMatKhau extends javax.swing.JDialog {
+public class QuenMatKhau extends javax.swing.JDialog implements Runnable{
 
     /**
      * Creates new form QuenMatKhau
@@ -22,6 +33,8 @@ public class QuenMatKhau extends javax.swing.JDialog {
         txtEmail.setBorder(BorderFactory.createCompoundBorder(txtEmail.getBorder(), BorderFactory.createEmptyBorder(0, 5, 0, 0)));
         txtMaXacNhan.setBorder(BorderFactory.createCompoundBorder(txtMaXacNhan.getBorder(), BorderFactory.createEmptyBorder(0, 5, 0, 0)));
         txtMatKhauMoi.setBorder(BorderFactory.createCompoundBorder(txtMatKhauMoi.getBorder(), BorderFactory.createEmptyBorder(0, 5, 0, 0)));
+        
+        btnXacNhan.setVisible(false);
     }
 
     /**
@@ -33,7 +46,7 @@ public class QuenMatKhau extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        btnGuiMaXacNhan = new javax.swing.JButton();
         pictureBox2 = new com.ui.swing.PictureBox();
         pictureBox1 = new com.ui.swing.PictureBox();
         txtMatKhauMoi = new javax.swing.JPasswordField();
@@ -43,7 +56,7 @@ public class QuenMatKhau extends javax.swing.JDialog {
         txtEmail = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         btnXacNhan = new com.ui.swing.component.Tab();
-        jLabel7 = new javax.swing.JLabel();
+        lblXacNhan = new javax.swing.JLabel();
         btnTroVeDangNhap = new com.ui.swing.component.Tab();
         jLabel6 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
@@ -52,8 +65,13 @@ public class QuenMatKhau extends javax.swing.JDialog {
         setTitle("Quên mật khẩu");
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Gửi mã xác nhận");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(443, 180, 120, 30));
+        btnGuiMaXacNhan.setText("Gửi mã xác nhận");
+        btnGuiMaXacNhan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuiMaXacNhanActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnGuiMaXacNhan, new org.netbeans.lib.awtextra.AbsoluteConstraints(433, 180, 130, 30));
 
         pictureBox2.setImage(new javax.swing.ImageIcon(getClass().getResource("/com/happywedding/assets/QuenMatKhau.png"))); // NOI18N
         getContentPane().add(pictureBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, -20, 420, 160));
@@ -90,10 +108,15 @@ public class QuenMatKhau extends javax.swing.JDialog {
 
         btnXacNhan.setEndColor("#FFB9B9");
         btnXacNhan.setStartColor("#E9D5CA");
+        btnXacNhan.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnXacNhanMouseClicked(evt);
+            }
+        });
 
-        jLabel7.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(102, 102, 102));
-        jLabel7.setText("Xác nhận");
+        lblXacNhan.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        lblXacNhan.setForeground(new java.awt.Color(102, 102, 102));
+        lblXacNhan.setText("Xác nhận");
 
         javax.swing.GroupLayout btnXacNhanLayout = new javax.swing.GroupLayout(btnXacNhan);
         btnXacNhan.setLayout(btnXacNhanLayout);
@@ -101,12 +124,12 @@ public class QuenMatKhau extends javax.swing.JDialog {
             btnXacNhanLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(btnXacNhanLayout.createSequentialGroup()
                 .addGap(54, 54, 54)
-                .addComponent(jLabel7)
+                .addComponent(lblXacNhan)
                 .addContainerGap(54, Short.MAX_VALUE))
         );
         btnXacNhanLayout.setVerticalGroup(
             btnXacNhanLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+            .addComponent(lblXacNhan, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
         );
 
         getContentPane().add(btnXacNhan, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 580, 200, 50));
@@ -152,6 +175,106 @@ public class QuenMatKhau extends javax.swing.JDialog {
         new DangNhap(new JFrame(), true).setVisible(true);
     }//GEN-LAST:event_btnTroVeDangNhapMouseClicked
 
+    Thread t1 = new Thread(this);
+    
+    private void btnGuiMaXacNhanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuiMaXacNhanActionPerformed
+            
+        if(!txtEmail.getText().equals("baomqpc03196@fpt.edu.vn")){
+            JOptionPane.showMessageDialog(this, "Sai email");      
+            return;
+        }
+        
+        txtEmail.setEditable(false);
+        t1.start();
+        
+    }//GEN-LAST:event_btnGuiMaXacNhanActionPerformed
+
+    private void btnXacNhanMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnXacNhanMouseClicked
+        JOptionPane.showMessageDialog(this, "Đổi mật khẩu thành công");
+        this.dispose();
+        new DangNhap(new JFrame(), true).setVisible(true);
+    }//GEN-LAST:event_btnXacNhanMouseClicked
+   
+    String taiKhoan = "baomqpc03196@fpt.edu.vn";
+    String matKhau = "matkhaulagidayta";
+    String guiDen = "baomqpc03196@fpt.edu.vn";
+    String tieuDe = "Mã xác minh tài khoản email";
+    
+    int randomNumber = 0;
+    
+    
+    public void goiMail(){
+            
+        int ranNum = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        randomNumber = ranNum;
+        
+        try{
+            //Các thông số kết nối tới mail Server
+            Properties prop = new Properties(); 
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.port", "587");
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls.enable", "true");
+            
+            //Tạo đối tượng Session đưa vào các thông tin xác thực tài khoản email
+            Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(taiKhoan, matKhau);
+                    }
+                });
+            //lấy các thông tin người dùng nhập vào trên Form
+            String from = taiKhoan;
+            String to = guiDen;
+            String subject = tieuDe;
+            String body = "Mã xác minh tài khoản email của bạn là: " + String.valueOf(ranNum);
+            //Tạo đối tượng MimeMessage
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients
+            (
+                Message.RecipientType.TO,
+                InternetAddress.parse(to)
+            );
+            message.setSubject(subject);
+            message.setText(body);
+            Transport.send(message); //gọi phương thức send để gửi message đi
+            JOptionPane.showMessageDialog(this, "Đã gởi mail thành công");
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void run(){
+        goiMail();
+        btnGuiMaXacNhan.setEnabled(false);
+        int giaTri = 61;
+        btnXacNhan.setEnabled(true);
+        while(true){
+            try {
+                // … mã lấy thời gian hệ thống hiển thị lên nút
+                Thread.sleep(1000);  
+                giaTri--;
+                btnGuiMaXacNhan.setText("Gửi lại sau " + giaTri + "s");
+                //JOptionPane.showMessageDialog(this, randomNumber);
+                if(txtMaXacNhan.getText().equals(String.valueOf(randomNumber))){
+                    btnXacNhan.setVisible(true);
+                }
+                if(giaTri == 0){
+                    btnXacNhan.setEnabled(false);
+                    btnGuiMaXacNhan.setText("Gửi mã xác nhận");
+                    btnGuiMaXacNhan.setEnabled(true);
+                    t1.stop();
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -195,15 +318,15 @@ public class QuenMatKhau extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGuiMaXacNhan;
     private com.ui.swing.component.Tab btnTroVeDangNhap;
     private com.ui.swing.component.Tab btnXacNhan;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel lblXacNhan;
     private com.ui.swing.PictureBox pictureBox1;
     private com.ui.swing.PictureBox pictureBox2;
     private javax.swing.JTextField txtEmail;
